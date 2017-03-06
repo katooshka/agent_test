f4c902a61137867ca97a0211c1cc56dd647f9bc1
katooshka <ek.zabara@yandex.ru>
2017-03-06 06:42:25 +0000

    Small fix to validator test

--- src/test/java/cli/ArgumentsValidatorTest.java
+++ src/test/java/cli/ArgumentsValidatorTest.java
@@ -3,8 +3,8 @@ package cli;
 import org.junit.Test;
 
 import static cli.ArgumentsValidator.validateArguments;
-import static com.sun.tools.internal.ws.wsdl.parser.Util.fail;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class ArgumentsValidatorTest {
 
